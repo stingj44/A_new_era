1d78ef3795ac62b5c85821fa1351e69d757c7e59
StingJ <justinsaechao23@gmail.com>
2025-03-18 16:32:35 -0700

    added rest of inputs for album

--- C_stuff/struc.cpp
+++ C_stuff/struc.cpp
@@ -11,14 +11,29 @@ struct Record {
 
 //Add a new record function
 int add_record() {
-	char name[40];
+	char name[40] = {};
+	char artist[40] = {};
+	int year = 0;
 	Record album;
 
-	cout << "Enter an album name: ";
-	cin.getline(name,40);
+	//Album name
+	cout << "Enter the album name: ";
+	cin.getline(name, 40);
 	strcpy(album.name, name);
+	
+	//Artist name
+	cout << endl << "Enter the artist name: ";
+	cin.getline(artist, 40);
+	strcpy(album.artist, artist);
 
-	cout << endl << album.name;
+	//Release year
+	cout << endl << "Enter the album release year: ";
+	cin >> album.release_yr;
+
+	//Display results (to be rmd)
+	cout << endl << "Album: " << album.name;
+	cout << endl << "Artist: " << album.artist;
+	cout << endl << "Released: " << album.release_yr << endl;
 
 	return 0;
 }
